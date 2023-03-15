74985875218d86f8b3da72e1b38d589ebe4990fe
Amit Kumar Singh <120605843+ranaAmittomar@users.noreply.github.com>
2023-03-15 15:18:40 +0530

    Balancing Brackets by maintaining Stack

--- Stack_InterViewQuestion_EASY_med_diff/Stack_InterViewQuestion_EASY_med_diff.cpp
+++ Stack_InterViewQuestion_EASY_med_diff/Stack_InterViewQuestion_EASY_med_diff.cpp
@@ -7,15 +7,12 @@
 using namespace std;
 
 
-
-
-
-int main()
+void balancingBrackets1()
 {
-    //  QUESTION TO CHECK WHETHER THE STRING IS VALID OR NOT....
+    //  QUESTION TO CHECK WHETHER THE STRING IS VALID OR NOT.... (QUE 1:-BALANCING BRACKETS)
     string s;
     cin >> s;
-    int i=0, count = 0;
+    int i = 0, count = 0;
 
 
 
@@ -24,14 +21,14 @@ int main()
         if (s[i] == '(')
             count++;
 
-        else if(s[i] == ')')
+        else if (s[i] == ')')
             count--;
 
 
         if (count < 0)
         {
-            cout << "invalid Sequence"<<endl;
-            return 0  ;
+            cout << "invalid Sequence" << endl;
+            return;
         }
 
         i++;
@@ -49,5 +46,56 @@ int main()
     }
 
 
+
+}
+
+
+void balancingBrackets2() //balancing Brackets using Stack(by maintaing stack for every pair....)
+{
+
+    stack<char> st;
+    string s;
+    cin >> s;
+    int i = 0; 
+    while (i < s.length())
+    {
+        if (s[i] == '{' || s[i] == '[' || s[i] == '(' || s[i] == '<')
+        {
+            st.push(s[i]);
+        }
+        else
+        {
+            if (st.empty())
+            {
+                cout << "Invalid Sequence" << endl;
+                return;
+            }
+            else if ((st.top() == '{' && s[i] == '}') ||
+                (st.top() == '[' && s[i] == ']') ||
+                (st.top() == '(' && s[i] == ')') ||
+                (st.top() == '<' && s[i] == '>')
+                )
+            {
+                st.pop();
+            }
+        }
+        i++;
+
+    }
+    if (st.empty())
+    {
+        cout << "Valid Sequence" << endl;
+    }
+    else
+    {
+        cout << "Invalid Sequence" << endl;
+    }
+
+}
+
+int main()
+{
+    
+    balancingBrackets2();
     return 0;
 }
